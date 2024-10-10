2ff6f599d7839f301705b7c9ac01089dc20863dc
Jofre Tomas <jofretomas9@gmail.com>
2024-10-10 15:52:00 -0300

    add cadastro de produtos

--- init.py
+++ init.py
@@ -1,26 +1,27 @@
 import pyautogui;
 import time;
+import pandas;
 
-#--------------------------------
+#----------------------------------
 
 pyautogui.PAUSE = 0.5
 
-#--------------------------------
+#----------------------------------
 
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 pyautogui.click(x=689, y=477)
 
-#--------------------------------
+#----------------------------------
 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
 time.sleep(2)
 
-#--------------------------------
+#----------------------------------
 
-pyautogui.click(x=1215, y=352)
+pyautogui.press("tab")
 pyautogui.write("rakaialbion@gmail.com")
 pyautogui.press("tab")
 pyautogui.write("rakai123")
@@ -28,5 +29,33 @@ pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
 
-#--------------------------------
+#----------------------------------
 
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
+
+#----------------------------------
+
+pyautogui.press("tab")
+pyautogui.write("codigo")
+
+pyautogui.press("tab")
+pyautogui.write("Marca do Produto")
+
+pyautogui.press("tab")
+pyautogui.write("Tipo do Produto")
+
+pyautogui.press("tab")
+pyautogui.write("Categoria do Produto")
+
+pyautogui.press("tab")
+pyautogui.write("Preço Unitário do Produto")
+
+pyautogui.press("tab")
+pyautogui.write("Custo do Produto")
+
+pyautogui.press("tab")
+pyautogui.write("OBS")
+
+pyautogui.press("tab")
+pyautogui.press("enter")
